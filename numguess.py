1d7e24b2522dfcfb454c3edf71a1e0129e713be4
gacenti <wjsrktks@gmail.com>
2023-02-02 15:48:03 +0900

    feat: Added guess_num and Checking algorithms

--- numguess.py
+++ numguess.py
@@ -6,3 +6,16 @@ answer = randint(1,100)
 # Print answer(for debugging)
 print(answer)
 
+# Get user's name, guess
+user_name = input('Hello, there! What is your name? ')
+guess = int(input(f'Hello, {user_name}! Guess a number between 1~100 : '))
+# print to check
+print(name,guess)
+
+# Check the answer
+
+if guess == answer:
+    print ("Correct!")
+else:
+    print(f"You are wrong! the answer is {answer}")
+
